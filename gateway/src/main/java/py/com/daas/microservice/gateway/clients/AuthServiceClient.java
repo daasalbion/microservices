18e5ef8a94e896a11d568bf2ba989ff49e868d59
Derlis Arguello <derlisarguello01@gmail.com>
2023-08-25 15:27:15 -0400

    configurations updated

--- gateway/src/main/java/py/com/daas/microservice/gateway/clients/AuthServiceClient.java
+++ gateway/src/main/java/py/com/daas/microservice/gateway/clients/AuthServiceClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "auth-service", url = "http://localhost:9002")
+@FeignClient(value = "identity-service")
 public interface AuthServiceClient {
 
     @GetMapping("/api/auth/validate")
