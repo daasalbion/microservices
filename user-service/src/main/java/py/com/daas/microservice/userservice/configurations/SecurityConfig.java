731f37556b80f480088d31fb11ae5755382c1cc7
Derlis Arguello <derlisarguello01@gmail.com>
2023-09-15 17:38:45 -0400

    SecurityConfig for UserService updated

--- user-service/src/main/java/py/com/daas/microservice/userservice/configurations/SecurityConfig.java
+++ user-service/src/main/java/py/com/daas/microservice/userservice/configurations/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -14,12 +15,12 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.csrf().disable()
-                .authorizeHttpRequests()
-                .requestMatchers("/**").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .build();
+        return http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(r -> r.requestMatchers("/**")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated()
+                ).build();
     }
 
     @Bean
